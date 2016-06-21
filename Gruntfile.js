06cbdbe5392fc91d4c4738191061dbe0413c87a4
thatONEjustin <justinpaelmo@gmail.com>
2016-06-21 15:51:02 -0400

    adding in node/grunt init files

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+/*
+ * grunt-contrib-uglify
+ * http://gruntjs.com/
+ *
+ * Copyright (c) 2016 "Cowboy" Ben Alman, contributors
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        
+    });
+    
+}
